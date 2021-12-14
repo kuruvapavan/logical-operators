2ba9459b6f7273b1abf744cc300bda56656846ed
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-14 19:22:52 +0530

    u

---
+++ logicaloperators.js
@@ -0,0 +1,23 @@
+if(5<10 && 15<20 && 20<25){
+    console.log(true)
+}else{
+        console.log(false)
+    }
+
+    if(5<10 && 15>20 && 20<25){
+        console.log(true)
+    }else{
+            console.log(false)
+        }
+    
+        if(5<10 || 15>20 || 20<25){
+            console.log(true)
+        }else{
+                console.log(false)
+            }
+
+            if(5>10 || 15>20 || 20>25){
+                console.log(true)
+            }else{
+                    console.log(false)
+                }
